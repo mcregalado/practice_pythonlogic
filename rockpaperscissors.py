04e9cecbc56878accb0ddb26f4c340be8160869a
Rianne <mcsregalado@gmail.com>
2025-10-29 20:12:35 +0800

    Added final result function and fixed the round count

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,6 +1,6 @@
 import random
 round = 1
-ties = numround = total_rounds = computer_score = player_score = 0
+ties  = total_rounds = computer_score = player_score = 0
 rol = ["rock", "paper", "scissors"]
 
 beats = {
@@ -21,26 +21,51 @@ def compare(choose, comp_ch):
 		print("You lose!")
 		computer_score += 1
 
-while numround < 5:
-	try:
-		print(f"Round {round}")
-		choose = input("Choose rock, paper, or scissors: ").lower()
-		if choose not in rol:
-			raise ValueError
-		comp_ch = random.choice(rol)
-		print(f"Computer chose: {comp_ch}")
-		compare(choose, comp_ch)
-		print(f"Score - You: {player_score} | Computer: {computer_score}")
-		total_rounds += 1
-		numround += 1
-		
-		if computer_score == 3:
+def final_result():
+	global round, ties, total_rounds, computer_score, player_score
+	if player_score > computer_score:
+		print(f"Match Over! You won {player_score}-{computer_score}!")
+	elif player_score == computer_score:
+		print(f"Match Over! It's a tie at {player_score}-{computer_score}!")
+	else:
+		print(f"Match Over! Computer won {player_score}-{computer_score}!")
+	play = input(f"Total rounds: {total_rounds} \nTies: {ties} \n Play again? (yes/no): ")
+	if play.lower() == "yes":
+		round = 1
+		ties  = total_rounds = computer_score = player_score = 0
+		game()
+	if play.lower() == "no":
+		print("Thanks for playing!")
+		exit()
+
+def game():
+	global round, total_rounds, player_score, computer_score, ties
+	while round < 6:
+		try:
+			print(f"Round {round}")
+			choose = input("Choose rock, paper, or scissors: ").lower()
+			if choose not in rol:
+				raise ValueError
+			comp_ch = random.choice(rol)
+			print(f"Computer chose: {comp_ch}")
+			compare(choose, comp_ch)
 			print(f"Score - You: {player_score} | Computer: {computer_score}")
-			print("Computer wins the game!")
-			exit()
-		elif player_score == 3:
-			print(f"Score - You: {player_score} | Computer: {computer_score}")
-			print("You win the game!")
-			exit()
-	except ValueError:
-		print("Invalid input. Please enter rock, paper, or scissors.")
+			total_rounds += 1
+			round += 1
+			
+			if computer_score == 3:
+				print("Computer wins the game!")
+				final_result()
+				exit()
+			elif player_score == 3:
+				print("You win the game!")
+				final_result()
+				exit()
+			elif round == 6:
+				final_result()
+		except ValueError:
+			print("Invalid input. Please enter rock, paper, or scissors.")
+
+if __name__ == "__main__":
+	game()
+
